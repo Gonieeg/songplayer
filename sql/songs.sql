1baf750702af3be9678c6c5191bb82e5b74ceb40
Gonieeg <goniee.g@gmail.com>
2026-01-23 12:56:30 +0100

    translated names, added 2 functions
    + view and get_all_songs
    + get_versions_for_song

--- sql/songs.sql
+++ sql/songs.sql
@@ -1,301 +1,338 @@
 ------------------ FUNKCJE DOT. PIOSENEK ------------------
 -- DODAJ PIOSENKE
 CREATE OR REPLACE FUNCTION add_song(
-    artysci VARCHAR(255)[],      -- max 3 artystow
-    tytul VARCHAR(255),
+    artists VARCHAR(255)[],      -- max 3 artystow
+    songtitle VARCHAR(255),
     album VARCHAR(255),
-    jezyk VARCHAR(255),
-    rok_wydania INTEGER,
-    gatunki INTEGER[],           -- max 3 gatunki, zbieramy "wektor"
-    wersja VARCHAR(255),
-    ile_trwa INTEGER,
-    data_wersji DATE
+    lang VARCHAR(255),
+    year_published INTEGER,
+    genres VARCHAR(255)[],           -- max 3 gatunki, zbieramy "wektor" nazw
+    songversion VARCHAR(255),
+    dur INTEGER,
+    version_date DATE
 )
 RETURNS TEXT
 LANGUAGE plpgsql
 AS $$
 DECLARE
-    id_artysty_albumu INTEGER; 	-- ID pierwszego artysty przypisanego do albumu 
-    id_albumu INTEGER;
-    id_jezyka INTEGER; 			-- Languages
-    id_piosenki INTEGER;		-- Songs
-    id_typu_wersji INTEGER;		-- VersionTypes
-    nazwa_artysty VARCHAR(255);	-- do petli
-    id_artysty INTEGER;			-- do petli
-	nazwa_gatunku VARCHAR(255);	-- do petli
-    id_gatunku INTEGER;			-- do petli
+    albumartid INTEGER; 	-- ID pierwszego artysty przypisanego do albumu 
+    albumid INTEGER;
+    langid INTEGER; 			-- Languages
+    sid INTEGER;		-- Songs (song id)
+    vtid INTEGER;		-- VersionTypes (id_typu_wersji)
+    artist_name VARCHAR(255);	-- do petli
+    artid INTEGER;			-- do petli
+	genre_name VARCHAR(255);	-- do petli
+    gid INTEGER;			-- do petli (genre id)
 BEGIN
     -- USTAWIENIE LIMITOW
 	-- max 3 artystow
-    IF artysci IS NULL OR array_length(artysci, 1) IS NULL OR array_length(artysci, 1) = 0 THEN
+    IF artists IS NULL OR array_length(artists, 1) IS NULL OR array_length(artists, 1) = 0 THEN
         RAISE EXCEPTION 'Musisz podac co najmniej 1 artyste.';
     END IF;
-    IF array_length(artysci, 1) > 3 THEN
+    IF array_length(artists, 1) > 3 THEN
         RAISE EXCEPTION 'Maksymalnie 3 artystow.';
     END IF;
 	-- max 3 gatunki
-    IF gatunki IS NULL OR array_length(gatunki, 1) IS NULL OR array_length(gatunki, 1) = 0 THEN
+    IF genres IS NULL OR array_length(genres, 1) IS NULL OR array_length(genres, 1) = 0 THEN
         RAISE EXCEPTION 'Musisz podac co najmniej 1 gatunek.';
     END IF;
-    IF array_length(gatunki, 1) > 3 THEN
+    IF array_length(genres, 1) > 3 THEN
         RAISE EXCEPTION 'Maksymalnie 3 gatunki.';
     END IF;
 	-- upewniamy sie ze czas trwania jest ok
-    IF ile_trwa IS NULL OR ile_trwa <= 0 THEN
+    IF dur IS NULL OR dur <= 0 THEN
         RAISE EXCEPTION 'Czas trwania musi byc > 0.';
     END IF;
 	-- pierwsza nagrana piosenka "Au Clair de la Lune", nie moze byc wczesniejszy rok
-    IF rok_wydania IS NULL OR rok_wydania <= 1860 THEN
+    IF year_published IS NULL OR year_published <= 1860 THEN
         RAISE EXCEPTION 'Rok wydania musi byc > 1860.';
     END IF;
 
     -- JEZYK: istnieje -> wpisujemy, nie istnieje -> dodajemy ID i nazwe 
     INSERT INTO Languages(language)
-    VALUES (jezyk)
+    VALUES (lang)
     ON CONFLICT (language) DO UPDATE SET language = EXCLUDED.language
-    RETURNING language_id INTO id_jezyka;
+    RETURNING language_id INTO langid;
 
     -- ARTYSTA ALBUMU: istnieje -> pierwszy z listy, nie istnieje -> dodaj
-    nazwa_artysty := artysci[1];
+    artist_name := artists[1];
     INSERT INTO Artists(name)
-    VALUES (nazwa_artysty)
+    VALUES (artist_name)
     ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name
-    RETURNING artist_id INTO id_artysty_albumu;
+    RETURNING artist_id INTO albumartid;
 
     -- ALBUM: istnieje -> wpisujemy, nie istnieje -> dodaj
     INSERT INTO MusicAlbums(title, artist_id, release_year)
-    VALUES (album, id_artysty_albumu, rok_wydania)
+    VALUES (album, albumartid, year_published)
     ON CONFLICT (title, artist_id) DO UPDATE SET release_year = EXCLUDED.release_year
-    RETURNING album_id INTO id_albumu;
+    RETURNING album_id INTO albumid;
 
     -- PIOSENKA: istnieje -> wpisujemy, nie istnieje -> dodaj
     INSERT INTO Songs(title, album_id, release_year, language_id)
-    VALUES (tytul, id_albumu, rok_wydania, id_jezyka)
+    VALUES (songtitle, albumid, year_published, langid)
     ON CONFLICT (title, album_id) DO UPDATE
       SET release_year = EXCLUDED.release_year,
           language_id  = EXCLUDED.language_id
-    RETURNING Songs.song_id INTO id_piosenki;
+    RETURNING Songs.song_id INTO sid;
 
     -- ARTYSCI: loop dla i<=3, nie istnieja -> dodaj + relacja
-    FOREACH nazwa_artysty IN ARRAY artysci LOOP
+    FOREACH artist_name IN ARRAY artists LOOP
         INSERT INTO Artists(name)
-        VALUES (nazwa_artysty)
+        VALUES (artist_name)
         ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name
-        RETURNING artist_id INTO id_artysty;
+        RETURNING artist_id INTO artid;
 
         INSERT INTO SongsArtists(song_id, artist_id)
-        VALUES (id_piosenki, id_artysty)
+        VALUES (sid, artid)
         ON CONFLICT DO NOTHING;
     END LOOP;
 
     -- GATUNKI: loop dla i<=3, nie istnieja -> dodaj + relacja
-    FOREACH nazwa_gatunku IN ARRAY gatunki LOOP
+    FOREACH genre_name IN ARRAY genres LOOP
         INSERT INTO MusicGenres(genre)
-        VALUES (nazwa_gatunku)
+        VALUES (genre_name)
         ON CONFLICT (genre) DO UPDATE SET genre = EXCLUDED.genre
-        RETURNING genre_id INTO id_gatunku;
+        RETURNING genre_id INTO gid;
 
         INSERT INTO SongsGenres(song_id, genre_id)
-        VALUES (id_piosenki, id_gatunku)
+        VALUES (sid, gid)
         ON CONFLICT DO NOTHING;
     END LOOP;
 
     -- WERSJA: nie istnieja -> dodaj + relacja
     INSERT INTO VersionTypes(name)
-	VALUES (wersja)
+	VALUES (songversion)
 	ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name
-	RETURNING version_type_id INTO id_typu_wersji;
+	RETURNING version_type_id INTO vtid;
 
     INSERT INTO SongVersions(song_id, version_type_id, duration, created_at)
-	VALUES (id_piosenki, id_typu_wersji, ile_trwa, data_wersji)
+	VALUES (sid, vtid, dur, version_date)
 	ON CONFLICT (song_id, version_type_id, created_at) DO UPDATE
 		SET duration = EXCLUDED.duration;
 
-    RETURN 'Dodano/uzupelniono piosenke (song_id=' || id_piosenki || ').';
+    RETURN 'Dodano/uzupelniono piosenke (song_id=' || sid || ').';
 END;
 $$;
 
 -- USUŃ PIOSENKE
-CREATE OR REPLACE FUNCTION delete_song(id_piosenki INTEGER)
+CREATE OR REPLACE FUNCTION delete_song(sid INTEGER)
 RETURNS TEXT
 LANGUAGE plpgsql
 AS $$
 DECLARE ile_wersji INTEGER;
 BEGIN
     -- sprawdzamy czy piosenka istnieje
-    IF NOT EXISTS (SELECT 1 FROM Songs WHERE song_id = id_piosenki) THEN
-        RETURN 'Nie ma piosenki o id=' || id_piosenki || '.';
+    IF NOT EXISTS (SELECT 1 FROM Songs WHERE song_id = sid) THEN
+        RETURN 'Nie ma piosenki o id=' || sid || '.';
     END IF;
 
     -- usuwamy najpierw jej relacje z artystą i gatunkiem
-    DELETE FROM SongsArtists WHERE song_id = id_piosenki;
-    DELETE FROM SongsGenres  WHERE song_id = id_piosenki;
+    DELETE FROM SongsArtists WHERE song_id = sid;
+    DELETE FROM SongsGenres  WHERE song_id = sid;
 
     -- liczymy ile wersji usunie sie kaskadowo
     SELECT COUNT(*) INTO ile_wersji
     FROM SongVersions
-    WHERE song_id = id_piosenki;
+    WHERE song_id = sid;
 
     -- usun (tez z SongVersions, PlaylistItems i ListeningHistory przez kaskady)
     DELETE FROM Songs
-    WHERE song_id = id_piosenki;
+    WHERE song_id = sid;
 
-    RETURN 'Usunieto piosenke id=' || id_piosenki || ' oraz jej ' || ile_wersji || ' wersji.';
+    RETURN 'Usunieto piosenke id=' || sid || ' oraz jej ' || ile_wersji || ' wersji.';
 END;
 $$;
 
 -- ZMODYFIKUJ PIOSENKE
 CREATE OR REPLACE FUNCTION modify_song(
-    id_piosenki INTEGER,
-    nowy_jezyk VARCHAR(255) DEFAULT NULL,
+    sid INTEGER,
+    new_lang VARCHAR(255) DEFAULT NULL,
 
-    dodaj_artyste VARCHAR(255) DEFAULT NULL,
-    usun_artyste VARCHAR(255) DEFAULT NULL,
+    add_artist VARCHAR(255) DEFAULT NULL,
+    delete_artist VARCHAR(255) DEFAULT NULL,
 
-    dodaj_gatunek VARCHAR(255) DEFAULT NULL,
-    usun_gatunek VARCHAR(255) DEFAULT NULL
+    add_genre VARCHAR(255) DEFAULT NULL,
+    delete_genre VARCHAR(255) DEFAULT NULL
 )
 RETURNS TEXT
 LANGUAGE plpgsql
 AS $$
 DECLARE
-    id_jezyka INTEGER;
-    id_artysty INTEGER;
-    id_gatunku INTEGER;
+    langid INTEGER;
+    artid INTEGER;
+    gid INTEGER;
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM Songs WHERE song_id = id_piosenki) THEN
-        RETURN 'Nie ma piosenki o id=' || id_piosenki || '.';
+    IF NOT EXISTS (SELECT 1 FROM Songs WHERE song_id = sid) THEN
+        RETURN 'Nie ma piosenki o id=' || sid || '.';
     END IF;
 
     -- ZMIANA JEZYKA (opcjonalne, nie istnieje -> dodajemy)
-    IF nowy_jezyk IS NOT NULL THEN
+    IF new_lang IS NOT NULL THEN
         INSERT INTO Languages(language)
-        VALUES (nowy_jezyk)
+        VALUES (new_lang)
         ON CONFLICT (language) DO UPDATE SET language = EXCLUDED.language
-        RETURNING language_id INTO id_jezyka;
+        RETURNING language_id INTO langid;
 
         UPDATE Songs
-        SET language_id = id_jezyka
-        WHERE song_id = id_piosenki;
+        SET language_id = langid
+        WHERE song_id = sid;
     END IF;
 
     -- DODAJ ARTYSTE (opcjonalne, nie istnieje -> dodajemy)
-    IF dodaj_artyste IS NOT NULL THEN
+    IF add_artist IS NOT NULL THEN
         INSERT INTO Artists(name)
-        VALUES (dodaj_artyste)
+        VALUES (add_artist)
         ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name
-        RETURNING artist_id INTO id_artysty;
+        RETURNING artist_id INTO artid;
 
         INSERT INTO SongsArtists(song_id, artist_id)
-        VALUES (id_piosenki, id_artysty)
+        VALUES (sid, artid)
         ON CONFLICT DO NOTHING;
     END IF;
 
     -- USUN ARTYSTE (opcjonalne)
-    IF usun_artyste IS NOT NULL THEN
-        SELECT artist_id INTO id_artysty
+    IF delete_artist IS NOT NULL THEN
+        SELECT artist_id INTO artid
         FROM Artists
-        WHERE name = usun_artyste
+        WHERE name = delete_artist
         LIMIT 1;
 		
-        IF id_artysty IS NOT NULL THEN
+        IF artid IS NOT NULL THEN
             DELETE FROM SongsArtists
-            WHERE song_id = id_piosenki AND artist_id = id_artysty;
+            WHERE song_id = sid AND artist_id = artid;
         END IF;
     END IF;
 
     -- DODAJ GATUNEK (opcjonalne, nie istnieje -> dodajemy)
-    IF dodaj_gatunek IS NOT NULL THEN
+    IF add_genre IS NOT NULL THEN
         INSERT INTO MusicGenres(genre)
-        VALUES (dodaj_gatunek)
+        VALUES (add_genre)
         ON CONFLICT (genre) DO UPDATE SET genre = EXCLUDED.genre
-        RETURNING genre_id INTO id_gatunku;
+        RETURNING genre_id INTO gid;
         INSERT INTO SongsGenres(song_id, genre_id)
-        VALUES (id_piosenki, id_gatunku)
+        VALUES (sid, gid)
         ON CONFLICT DO NOTHING;
     END IF;
 
     -- USUN GATUNEK (opcjonalne)
-    IF usun_gatunek IS NOT NULL THEN
-        SELECT genre_id INTO id_gatunku
+    IF delete_genre IS NOT NULL THEN
+        SELECT genre_id INTO gid
         FROM MusicGenres
-        WHERE genre = usun_gatunek
+        WHERE genre = delete_genre
         LIMIT 1;
-        IF id_gatunku IS NOT NULL THEN
+        IF gid IS NOT NULL THEN
             DELETE FROM SongsGenres
-            WHERE song_id = id_piosenki AND genre_id = id_gatunku;
+            WHERE song_id = sid AND genre_id = gid;
         END IF;
     END IF;
 
-    RETURN 'Zmodyfikowano piosenke o id=' || id_piosenki || '.';
+    RETURN 'Zmodyfikowano piosenke o id=' || sid || '.';
 END;
 $$;
 
 -- DODAJ NOWA WERSJE PIOSENKI
 CREATE OR REPLACE FUNCTION add_version(
-    id_piosenki INTEGER,
-    wersja VARCHAR(255),
-    ile_trwa INTEGER,
-    data_wersji DATE DEFAULT CURRENT_DATE
+    sid INTEGER,
+    songversion VARCHAR(255),
+    dur INTEGER,
+    version_date DATE DEFAULT CURRENT_DATE
 )
 RETURNS TEXT
 LANGUAGE plpgsql
 AS $$
 DECLARE
-    id_typu_wersji INTEGER;
-    id_wersji INTEGER;
+    vtid INTEGER;
+    vid INTEGER;
 BEGIN
     -- sprawdzamy czy piosenka istnieje
-    IF NOT EXISTS (SELECT 1 FROM Songs WHERE song_id = id_piosenki) THEN
-        RETURN 'Nie ma piosenki o id=' || id_piosenki || '.';
+    IF NOT EXISTS (SELECT 1 FROM Songs WHERE song_id = sid) THEN
+        RETURN 'Nie ma piosenki o id=' || sid || '.';
     END IF;
 
     -- WERSJA: nie istnieje -> dodaj typ
     INSERT INTO VersionTypes(name)
-    VALUES (wersja)
+    VALUES (songversion)
     ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name
-    RETURNING version_type_id INTO id_typu_wersji;
+    RETURNING version_type_id INTO vtid;
 
     -- upewniamy sie ze czas trwania jest ok
-    IF ile_trwa IS NULL OR ile_trwa <= 0 THEN
+    IF dur IS NULL OR dur <= 0 THEN
         RAISE EXCEPTION 'Czas trwania musi byc > 0.';
     END IF;
 
     -- mamy UNIQUE(song_id, version_type_id, created_at) dodajemy/uzupelniamy
     INSERT INTO SongVersions(song_id, version_type_id, duration, created_at)
-    VALUES (id_piosenki, id_typu_wersji, ile_trwa, data_wersji)
+    VALUES (sid, vtid, dur, version_date)
     ON CONFLICT (song_id, version_type_id, created_at) DO UPDATE
       SET duration = EXCLUDED.duration
-    RETURNING song_version_id INTO id_wersji;
+    RETURNING song_version_id INTO vid;
 
     RETURN 'Dodano nowa wersje piosenki.';
 END;
 $$;
 
 -- USUN WERSJE PIOSENKI
-CREATE OR REPLACE FUNCTION delete_version(id_wersji INTEGER)
+CREATE OR REPLACE FUNCTION delete_version(vid INTEGER)
 RETURNS TEXT
 LANGUAGE plpgsql
 AS $$
 DECLARE
-    id_piosenki INTEGER;
+    sid INTEGER;
     ile_wersji INTEGER;
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM SongVersions WHERE song_version_id = id_wersji) THEN
-        RETURN 'Nie ma wersji o id=' || id_wersji || '.';
+    IF NOT EXISTS (SELECT 1 FROM SongVersions WHERE song_version_id = vid) THEN
+        RETURN 'Nie ma wersji o id=' || vid || '.';
     END IF;
 
     DELETE FROM SongVersions
-    WHERE song_version_id = id_wersji;
+    WHERE song_version_id = vid;
 
     RETURN 'Usunieto wersje piosenki.';
 END;
 $$;
 
--- 2. Pobieranie opcji do wyszukiwarki utworów
-CREATE OR REPLACE FUNCTION get_song_version_choices()
-RETURNS TABLE(id INTEGER, display_label TEXT) AS $$
+
+-- Zwracania wszystkich wersji danego tytułu
+CREATE OR REPLACE FUNCTION get_versions_for_song(p_title VARCHAR)
+RETURNS TABLE (
+    song_id INTEGER,
+    song_title VARCHAR,
+    album_title VARCHAR,
+    artist_name VARCHAR,
+    version_type VARCHAR,
+    duration INTEGER,
+    created_at DATE
+)
+LANGUAGE plpgsql
+AS $$
 BEGIN
+    RETURN QUERY
+    SELECT
+        s.song_id,
+        s.title AS song_title,
+        ma.title AS album_title,
+        ar.name AS artist_name,
+        vt.name AS version_type,
+        sv.duration,
+        sv.created_at
+    FROM Songs s
+    JOIN MusicAlbums ma ON ma.album_id = s.album_id
+    JOIN Artists ar ON ar.artist_id = ma.artist_id
+    JOIN SongVersions sv ON sv.song_id = s.song_id
+    JOIN VersionTypes vt ON vt.version_type_id = sv.version_type_id
+    WHERE s.title = p_title
+    ORDER BY sv.created_at;
+END;
+$$;
+
+
+-- Pobieranie opcji do wyszukiwarki utworów
+CREATE OR REPLACE FUNCTION get_song_version_choices()
+RETURNS TABLE(id INTEGER, display_label TEXT) 
+LANGUAGE plpgsql 
+AS $$ 
+BEGIN 
     RETURN QUERY 
     SELECT 
         sv.song_version_id, 
@@ -308,4 +345,42 @@ BEGIN
     GROUP BY sv.song_version_id, s.title, vt.name
     ORDER BY s.title;
 END;
-$$ LANGUAGE plpgsql;
+$$;
+
+
+--- wyświetl wszystkie piosenki (bibliotekę) - widok i funkcja z niego korzystająca 
+CREATE OR REPLACE VIEW view_all_songs AS
+SELECT
+    s.title AS song_title,
+    vt.name AS version_type,
+    ma.title AS album_title,
+    a.name AS artist_name,
+    sv.duration AS duration
+FROM SongVersions sv
+    JOIN Songs s USING (song_id)
+    JOIN MusicAlbums ma ON s.album_id = ma.album_id
+    JOIN SongsArtists sa ON sa.song_id = s.song_id
+    JOIN Artists a ON sa.artist_id = a.artist_id
+    JOIN VersionTypes vt USING (version_type_id)
+ORDER BY s.title;
+---- funkcja do R
+CREATE OR REPLACE FUNCTION get_all_songs()
+RETURNS TABLE(
+    song_title VARCHAR(255),
+    version_type VARCHAR(255),
+    album_title VARCHAR(255),
+    artist_name VARCHAR(255),
+    duration INT)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        song_title,
+        version_type,
+        album_title,
+        artist_name,
+        duration
+    FROM view_all_songs;
+END;
+$$;
