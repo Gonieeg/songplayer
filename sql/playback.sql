06024c7f92e91d27a80bcc07cc37a3b36139fd49
Nieznajomy367 <syskaszymon@gmail.com>
2026-01-25 16:09:19 +0100

    playback.sql

--- sql/playback.sql
+++ sql/playback.sql
@@ -69,4 +69,48 @@ BEGIN
 
     DELETE FROM PlaybackSessions WHERE session_id = p_session_id;
 END;
+
+$$;
+
+-- Nowa zakończenie odtwarzania
+CREATE OR REPLACE FUNCTION finish_playback(p_session_id INTEGER)
+RETURNS VOID
+LANGUAGE plpgsql
+AS $$
+  DECLARE
+v_sv_id INTEGER;
+v_total_sec INTEGER;
+v_dur INTEGER;
+v_session_active BOOLEAN;
+v_already_listened INTEGER;
+v_last_upd TIMESTAMPTZ;
+BEGIN
+-- Pobieramy dane sesji
+SELECT song_version_id, listened_seconds, last_update, is_active
+INTO v_sv_id, v_already_listened, v_last_upd, v_session_active
+FROM PlaybackSessions WHERE session_id = p_session_id;
+
+IF NOT FOUND THEN RETURN; END IF;
+
+-- Pobieramy długość utworu
+SELECT duration INTO v_dur FROM SongVersions WHERE song_version_id = v_sv_id;
+
+-- OBLICZANIE CZASU:
+  -- Jeśli sesja jest aktywna, dodajemy różnicę czasu od ostatniego update'u do teraz
+    IF v_session_active THEN
+        v_total_sec := v_already_listened + EXTRACT(EPOCH FROM (now() - v_last_upd))::INTEGER;
+    ELSE
+        v_total_sec := v_already_listened;
+    END IF;
+
+    -- Zabezpieczenie: nie możemy słuchać dłużej niż trwa utwór (np. przez błędy systemowe)
+    IF v_total_sec > v_dur THEN v_total_sec := v_dur; END IF;
+
+    -- Zapis do historii
+    INSERT INTO ListeningHistory(song_version_id, listened_at, listened_seconds, is_full_played)
+    VALUES (v_sv_id, now(), v_total_sec, v_total_sec >= v_dur * 0.8);
+
+    -- Usuwamy sesję
+    DELETE FROM PlaybackSessions WHERE session_id = p_session_id;
+END;
 $$;
